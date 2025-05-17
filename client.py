8e1d018d85ea239bfbd62350007a699f14ead8a9
Saleh Mohammadhosseini <salehmhosseini2002@gmail.com>
2025-05-17 22:49:09 +0330

    add the client name and save the history of messages.

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
 import socket
 import threading
+from datetime import datetime
+
+username = input("Enter your name: ")
 
 def receive_messages(sock):
     while True:
@@ -7,6 +10,7 @@ def receive_messages(sock):
             message = sock.recv(1024).decode()
             if message:
                 print("\n" + message)
+                save_message(message)
         except:
             print("[!] Connection lost.")
             sock.close()
@@ -15,11 +19,17 @@ def receive_messages(sock):
 def send_messages(sock):
     while True:
         message = input()
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        formatted = f"[{timestamp}] {username}: {message}"
         try:
-            sock.send(message.encode())
+            sock.send(formatted.encode())
         except:
             print("[!] Failed to send.")
             break
+
+def save_message(message):
+    with open("chat_history.txt", "a") as f:
+        f.write(message + "\n")
 
 def start_client(server_ip='127.0.0.1', port=12345):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
